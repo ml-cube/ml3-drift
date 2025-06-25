8023a91f19d2582ec37d79be9140076ba997e9a6
Alessandro Lavelli <alessandro.lavelli@mlcube.com>
2025-06-25 14:24:19 +0200

    abstract class for data batch analyzer

--- src/ml3_drift/analysis/analyzer/stream.py
+++ src/ml3_drift/analysis/analyzer/stream.py
@@ -1,32 +1,12 @@
 import numpy as np
-from typing import TYPE_CHECKING, Callable, TypeIs, Union
 
+from typing import Callable
+from ml3_drift.analysis.analyzer.base import DataDriftAnalyzer
 from ml3_drift.analysis.report import Report
 from ml3_drift.monitoring.base import MonitoringAlgorithm
 
-if TYPE_CHECKING:
-    import pandas as pd
-    import polars as pl
 
-POLARS = True
-try:
-    import polars as pl
-except ModuleNotFoundError:
-    POLARS = False
-
-
-PANDAS = True
-try:
-    import pandas as pd
-except ModuleNotFoundError:
-    PANDAS = False
-
-
-class DataDriftAnalyzer:
-    """
-    Analyze a dataset identifying the sequence of distributions due to data drifts.
-    """
-
+class StreamDataDriftAnalyzer(DataDriftAnalyzer):
     def __init__(
         self,
         continuous_ma_builder: Callable[[int], MonitoringAlgorithm],
@@ -34,58 +14,13 @@ class DataDriftAnalyzer:
         reference_size: int = 100,
         comparison_window_size: int = 100,
     ):
+        super().__init__(
+            continuous_ma_builder,
+            categorical_ma_builder,
+        )
+
         self.reference_size = reference_size
         self.comparison_window_size = comparison_window_size
-
-        self.continuous_ma_builder = continuous_ma_builder
-        self.categorical_ma_builder = categorical_ma_builder
-
-    def _is_list_str(self, columns: list[str] | list[int]) -> TypeIs[list[str]]:
-        """Verify if the input variable is a list of str in any element"""
-
-        return all(isinstance(elem, str) for elem in columns)
-
-    def _to_index(
-        self,
-        X: Union[np.ndarray, "pd.DataFrame", "pl.DataFrame"],
-        columns: list[str] | list[int] | None,
-    ) -> list[int]:
-        """Translate the list of columns in list of indices.
-
-        If columns is None then all the indexes are returned.
-        If columns is list[int] then it is directly returned.
-        If columns is list[str] then the indexes are retrieved from column names,
-        in this case X must be a DataFrame."""
-
-        if columns is None:
-            return list(range(X.shape[0]))
-
-        if self._is_list_str(columns):
-            if POLARS and isinstance(X, pl.DataFrame):
-                return [i for (i, c) in enumerate(X.columns) if c in columns]
-            elif PANDAS and isinstance(X, pd.DataFrame):
-                return [i for (i, c) in enumerate(X.columns) if c in columns]
-            else:
-                raise ValueError(
-                    "Type not valid, expecting polars DataFrame or pandas DataFrame when columns has string values. Got {type(X)}"
-                )
-        return columns
-
-    def _to_numpy(
-        self, X: Union[np.ndarray, "pd.DataFrame", "pl.DataFrame"]
-    ) -> np.ndarray:
-        """Transform input data into numpy array"""
-
-        if POLARS and isinstance(X, pl.DataFrame):
-            return X.to_numpy()
-        elif PANDAS and isinstance(X, pd.DataFrame):
-            return X.to_numpy()
-        elif isinstance(X, np.ndarray):
-            return X
-        else:
-            raise ValueError(
-                f"Type not valid, expecting numpy array, polars DataFrame or pandas DataFrame. Got {type(X)}"
-            )
 
     def _scan_data(
         self,
@@ -176,47 +111,3 @@ class DataDriftAnalyzer:
             concepts.append((concepts[-1][1], X.shape[0]))
 
         return Report(concepts)
-
-    def analyze(
-        self,
-        X: Union[np.ndarray, "pd.DataFrame", "pl.DataFrame"],
-        y: Union[None, np.ndarray, "pd.DataFrame", "pl.DataFrame"] = None,
-        continuous_columns: list[str] | list[int] | None = None,
-        categorical_columns: list[str] | list[int] | None = None,
-        y_categorical: bool = False,
-    ) -> Report:
-        # Shape check
-        if y is not None and X.shape[0] != y.shape[0]:
-            raise ValueError(
-                f"When target y is not None it must have the same rows of input X. Got X: {X.shape} and y: {y.shape}"
-            )
-
-        # Continuous and categorical columns to canonical form
-        if continuous_columns is not None:
-            continuous_columns_ids = self._to_index(X, continuous_columns)
-        else:
-            continuous_columns_ids = []
-
-        if categorical_columns is not None:
-            categorical_columns_ids = self._to_index(X, categorical_columns)
-        else:
-            categorical_columns_ids = []
-
-        # Input and target in canonical form
-        array_X = self._to_numpy(X)
-
-        if y is not None:
-            array_y = self._to_numpy(y)
-        else:
-            array_y = None
-
-        # Data analysis
-        report = self._scan_data(
-            array_X,
-            array_y,
-            continuous_columns_ids,
-            categorical_columns_ids,
-            y_categorical,
-        )
-
-        return report
